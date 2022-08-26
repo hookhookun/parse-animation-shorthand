201e309a0cd47116396f5733a618b7d6eadfc47a
Hookhoo <hookhookun@gmail.com>
2022-08-26 03:54:14 +0000

    test: add cases: (0.68, -0.82, 0.42, 1.52)

--- src/getCubicBezier.test.ts
+++ src/getCubicBezier.test.ts
@@ -28,6 +28,7 @@ const test = (
 
 test('(0,0,1,1)', 0, {start: 0, end: 9, value: [0, 0, 1, 1]});
 test('( 0.1 , 0.2 , 0.3 , 0.4 )', 0, {start: 0, end: 25, value: [0.1, 0.2, 0.3, 0.4]});
+test('( 0.68, -0.82, 0.42, 1.52 )', 0, {start: 0, end: 25, value: [0.68, -0.82, 0.42, 1.52]});
 test('( 0.1 , 0.2 , 0.3 , 0.4 ', 0, 'UnclosedParenthesis');
 test(' 0.1 , 0.2 , 0.3 , 0.4 )', 0, 'NoOpenParenthesis');
 test('(0,0,1 1)', 0, 'NoComma');
